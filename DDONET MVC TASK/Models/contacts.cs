3b24ab06ca07861ae4154f8690cd87d4a649e1cd
naveenkr113 <naveenkr113@gmail.com>
2021-06-07 12:53:29 +0530

    commit 2

---
+++ DDONET MVC TASK/Models/contacts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDONET_MVC_TASK.Models
+{
+    public class contacts
+    {
+        public Int32 cnt_id { get; set; }
+        public string fst_nam { get; set; }
+        public string lst_nam { get; set; }
+        public string cpny { get; set; }
+        public int prof_id { get; set; }
+    }
+}
